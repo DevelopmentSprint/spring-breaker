f1fd329c4fbd39d14ece137d290414e261b71952
Todd Orr <torr@developmentsprint.com>
2014-08-30 13:52:00 -0400

    Added in better support for handling fallbacks. Fixes #2.

--- src/main/java/com/developmentsprint/spring/breaker/hystrix/HystrixCircuitManager.java
+++ src/main/java/com/developmentsprint/spring/breaker/hystrix/HystrixCircuitManager.java
@@ -34,6 +34,7 @@ import com.developmentsprint.spring.breaker.hystrix.fallback.HystrixFallback;
 import com.developmentsprint.spring.breaker.interceptor.CircuitBreakerAttribute;
 import com.netflix.config.ConfigurationManager;
 import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommand.Setter;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandKey;
 import com.netflix.hystrix.HystrixThreadPoolKey;
@@ -89,31 +90,7 @@ public class HystrixCircuitManager implements CircuitManager {
                 .andCommandKey(HystrixCommandKey.Factory.asKey(circuitBreakerName))
                 .andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey(threadPoolName));
 
-        HystrixCommand<Object> command = new HystrixCommand<Object>(setter) {
-
-            @Override
-            protected Object run() throws Exception {
-                try {
-                    return invoker.invoke();
-                } catch (Exception e) {
-                    if (e.getCause() != null && e.getCause() instanceof Exception) {
-                        e = ((Exception)e.getCause());
-                    }
-                    throw e;
-                }
-            }
-
-            @Override
-            protected Object getFallback() {
-                if (fallback instanceof FailFastFallback) {
-                    return super.getFallback();
-                } else if (fallback instanceof FailSilentFallback) {
-                    return null;
-                } else {
-                    return fallback.fallback();
-                }
-            }
-        };
+        HystrixCommand<?> command = CommandFactory.getInstance(setter, invoker, fallback);
 
         try {
             return command.execute();
@@ -180,6 +157,67 @@ public class HystrixCircuitManager implements CircuitManager {
             return fallbackClass.getDeclaredConstructor().newInstance();
         } catch (Exception e) {
             throw new CircuitBreakerException(e.getMessage(), e);
+        }
+    }
+    
+    private static class CommandFactory {
+        
+        static <T> Command<T> getInstance(Setter setter, Invoker invoker, HystrixFallback<T> fallback) {
+            
+            if (fallback != null) {
+                return new CommandWithFallback<T>(setter, invoker, fallback);
+            } else {
+                return new Command<T>(setter, invoker);
+            }
+            
+        }
+        
+    }
+    
+    private static class Command<T> extends HystrixCommand<T> {
+        
+        private final Invoker invoker;
+
+        Command(Setter setter, Invoker invoker) {
+            super(setter);
+            this.invoker = invoker;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected T run() throws Exception {
+            try {
+                return (T) invoker.invoke();
+            } catch (Exception e) {
+                if (e.getCause() != null && e.getCause() instanceof Exception) {
+                    throw ((Exception)e.getCause());
+                }
+                throw e;
+            }
+        }
+
+    }
+    
+    private static class CommandWithFallback<T> extends Command<T> {
+        
+        private final HystrixFallback<T> fallback;
+
+        CommandWithFallback(Setter setter, Invoker invoker, HystrixFallback<T> fallback) {
+            super(setter, invoker);
+            this.fallback = fallback;
+        }
+
+        @Override
+        protected T getFallback() {
+            if (fallback instanceof FailFastFallback) {
+                return super.getFallback();
+            } else if (fallback instanceof FailSilentFallback) {
+                return null;
+            } else if (fallback != null) {
+                return fallback.fallback();
+            } else {
+                return null;
+            }
         }
     }
 
